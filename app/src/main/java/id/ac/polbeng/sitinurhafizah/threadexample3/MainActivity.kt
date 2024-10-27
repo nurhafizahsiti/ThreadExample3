8d424a013dc4f4079619bc0016574ced81610d9d
nurhafizahsiti <nurhafizahsiti27@gmail.com>
2024-10-27 15:53:54 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polbeng/sitinurhafizah/threadexample3/MainActivity.kt
@@ -0,0 +1,20 @@
+package id.ac.polbeng.sitinurhafizah.threadexample3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.polbeng.sitinurhafizah.threadexample3.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnStartThread.setOnClickListener {
+            startThread()
+        }
+    }
+    private fun startThread(){
+        val runnable = ExampleRunnable(10);
+        Thread(runnable).start()
+    }
+}
